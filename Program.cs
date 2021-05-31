c4ee11cc4bfa305615acea3d662d2b73cd0e7f19
PureLame <pure.lame.01@gmail.com>
2021-05-31 19:16:10 +0300

    Init

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IJunior
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float health;
+            int armor;
+            int damage;
+
+            Console.Write("Введите кол-во жизней: ");
+            health = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во брони: ");
+            armor = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите кол-во урона: ");
+            damage = Convert.ToInt32(Console.ReadLine());
+
+            health -= Convert.ToSingle(damage) / 100 * armor;
+
+            Console.WriteLine("После атаки в " + damage + " урона, у вас осталось " + health + " жизней.");
+            
+        }
+    }
+}
